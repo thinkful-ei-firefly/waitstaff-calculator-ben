636a066dced986b3ff83d9d79f0ff59aa5d75e3e
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-16 12:27:23 -0400

    make render stubs

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ const STORE = {
   earnings: { total: 0, count: 0, avgTip: 0 },
 };
 
+
+function renderCharges() {
+  $('.subtotal').text('0');
+  $('.tip').text('0');
+  $('.total').text('0');
+}
+
+function renderEarnings() {
+  $('.tip-total').text('0');
+  $('.meal-count').text('0');
+  $('.avg-tip').text('0');
+}
+
+function waitstaffCalculator() {
+  renderCharges();
+  renderEarnings();
+}
+
+$(waitstaffCalculator);
