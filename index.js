65a47438b21c27f945333d7714914e2c0e6b9156
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-16 13:28:08 -0400

    math fully functional

--- index.js
+++ index.js
@@ -3,30 +3,47 @@
 'use strict';
 
 const STORE = {
-  meal: { price: 0, tax: 0, tip: 0 },
+  mealCount: 0,
+  tipTotal: 0,
+  tips: [],
   charges: { subtotal: 0, tip: 0, total: 0 },
   earnings: { total: 0, count: 0, avgTip: 0 },
 };
 
+function getDecimalFromPercent(num) {
+  return num / 100;
+}
+
 function handleCharges(meal) {
   const {price, tax, tip} = meal;
-  const taxDecimal = tax / 100;
-  const subtotal = (price * taxDecimal) + price;
-  const tipAmount = (tip / 100) * price;
+  const subtotal = (price * tax) + price;
+  const tipAmount = tip * price;
   const total = subtotal + tipAmount;
+  STORE.tipTotal += tipAmount;
   renderCharges(subtotal, tipAmount, total);
+}
+
+function handleEarnings(price, tip) {
+  STORE.tips.push(price * tip);
+  const sumTip = STORE.tips.reduce((acc, cur) => acc + cur, 0);
+  const avgTip = sumTip / STORE.tips.length;
+  renderEarnings(avgTip);
 }
 
 function handleFormSubmit() {
   $('form').submit(event => {
     event.preventDefault();
+    STORE.mealCount++;
     let price = $(event.currentTarget).find('[name="price"]').val();
     let tax = $(event.currentTarget).find('[name="tax"]').val();
     let tip = $(event.currentTarget).find('[name="tip"]').val();
     price = parseFloat(price);
     tax = parseFloat(tax);
     tip = parseFloat(tip);
+    tax = getDecimalFromPercent(tax);
+    tip = getDecimalFromPercent(tip);
     handleCharges({price, tax, tip});
+    handleEarnings(price, tip);
   });
 }
 
@@ -36,16 +53,15 @@ function renderCharges(subtotal, tip, total) {
   $('.total').text(total);
 }
 
-function renderEarnings() {
-  $('.tip-total').text('0');
-  $('.meal-count').text('0');
-  $('.avg-tip').text('0');
+function renderEarnings(avgTip) {  
+  $('.tip-total').text(STORE.tipTotal);
+  $('.meal-count').text(STORE.mealCount);
+  $('.avg-tip').text(avgTip);
 }
 
 function waitstaffCalculator() {
   handleFormSubmit();
-  renderCharges();
-  renderEarnings();
+  renderEarnings(0);
 }
 
 $(waitstaffCalculator);
